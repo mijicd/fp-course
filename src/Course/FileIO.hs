a1d71c2e5b820b6b24e6de73b938b3fb78a6cf9e
Dejan Mijic <dmijic@acm.org>
2019-01-24 11:10:39 +0100

    Complete FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -79,52 +79,43 @@ the contents of c
 
 -}
 
--- Given the file name, and file contents, print them.
--- Use @putStrLn@.
-printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
-
--- Given a list of (file name and file contents), print each.
--- Use @printFile@.
-printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
-
--- Given a file name, return (file name and file contents).
--- Use @readFile@.
-getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
-
--- Given a list of file names, return list of (file name and file contents).
--- Use @getFile@.
-getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+-- /Tip:/ use @getArgs@ and @run@
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    Nil    -> putStrLn "no arguments"
+    x :. _ -> run x
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
-run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run :: FilePath -> IO ()
+run fp = do
+  listing <- readFile fp
+  files   <- getFiles $ lines listing
+  printFiles files
 
--- /Tip:/ use @getArgs@ and @run@
-main ::
-  IO ()
-main =
-  error "todo: Course.FileIO#main"
+-- Given a list of file names, return list of (file name and file contents).
+-- Use @getFile@.
+getFiles :: List FilePath -> IO (List (FilePath, Chars))
+getFiles fps = sequence $ getFile <$> fps
+
+-- Given a file name, return (file name and file contents).
+-- Use @readFile@.
+getFile :: FilePath -> IO (FilePath, Chars)
+getFile fp = ((,) fp) <$> readFile fp
+
+-- Given a list of (file name and file contents), print each.
+-- Use @printFile@.
+printFiles :: List (FilePath, Chars) -> IO ()
+printFiles fpcs = void . sequence $ uncurry printFile <$> fpcs
+
+-- Given the file name, and file contents, print them.
+-- Use @putStrLn@.
+printFile :: FilePath -> Chars -> IO ()
+printFile fp content = do
+  putStrLn $ "============ " ++ fp
+  putStrLn content
 
 ----
 
