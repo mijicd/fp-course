54f70b387f21307f95b2b50fdf16d3f73e07c4ba
Dejan Mijic <dmijic@acm.org>
2019-02-16 20:16:23 +0100

    Complete extend

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,7 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  (<<=) f eoa = ExactlyOne (f eoa)
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +50,10 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  (<<=) f as =
+    case as of
+      Nil      -> Nil
+      (_ :. t) -> f as :. (f <<= t)
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +67,7 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  (<<=) f = (f . Full <$>)
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +86,4 @@ cojoin ::
   Extend f =>
   f a
   -> f (f a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (<<=) id
