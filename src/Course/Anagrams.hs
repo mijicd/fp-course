8cb84f331e1c8dd3669cfdb7c05bd351b6f9cfdb
Dejan Mijic <dmijic@acm.org>
2019-03-02 01:02:53 +0100

    Complete anagrams

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -25,20 +25,12 @@ Functions that might help
 
 -}
 
-
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-anagrams ::
-  Chars
-  -> FilePath
-  -> IO (List Chars)
-anagrams =
-  error "todo: Course.Anagrams#anagrams"
+anagrams :: Chars -> FilePath -> IO (List Chars)
+anagrams str file =
+  intersectBy equalIgnoringCase (permutations str) . lines <$> readFile file
 
 -- Compare two strings for equality, ignoring case
-equalIgnoringCase ::
-  Chars
-  -> Chars
-  -> Bool
-equalIgnoringCase =
-  error "todo: Course.Anagrams#equalIgnoringCase"
+equalIgnoringCase :: Chars -> Chars -> Bool
+equalIgnoringCase = on (==) (toLower <$>)
